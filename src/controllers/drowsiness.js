c46bb96f58875152150c86aa2163bec1ab6dc8ff
Yuki XD <pxnditxyr@gmail.com>
2023-03-10 07:00:29 -0800

    fix: changed pin

--- src/controllers/drowsiness.js
+++ src/controllers/drowsiness.js
@@ -1,15 +1,15 @@
 import rpio from 'rpio';
 
 export const sendSignalToBrake = async ( _req, res ) => {
-  const PIN_FRENAR = 21;
-  rpio.open(PIN_FRENAR, rpio.OUTPUT, rpio.LOW);
+  const PIN_FRENAR = 40;
+  rpio.open( PIN_FRENAR, rpio.OUTPUT, rpio.LOW );
 
   for (var i = 0; i < 5; i++) {
-    rpio.write(PIN_FRENAR, rpio.HIGH);
-    rpio.sleep(1);
+    rpio.write( PIN_FRENAR, rpio.HIGH );
+    rpio.sleep( 1 );
 
-    rpio.write(PIN_FRENAR, rpio.LOW);
-    rpio.msleep(500);
+    rpio.write( PIN_FRENAR, rpio.LOW );
+    rpio.msleep( 500 );
   }
 
 };
